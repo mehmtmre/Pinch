2d69634e9d3d3486f2775935bcab457dd628af4d
Mehmet Emre Çimenlik <memrecm@gmail.com>
2024-07-13 19:04:52 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mehmet Emre Çimenlik on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
